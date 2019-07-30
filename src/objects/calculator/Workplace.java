69e932cd517690bbcdde6e2f42452e87bdf5ebc1
Ivan Alekseev <jegius@gmail.com>
2019-07-31 01:12:33 +0400

    Update Workplace.java

--- src/objects/calculator/Workplace.java
+++ src/objects/calculator/Workplace.java
@@ -3,7 +3,7 @@ package objects.calculator;
 public class Workplace {
     public static void start() {
         /*
-         * Здесь необходимо создать экземлпяр калькулятора и вывести в консль
+         * Здесь необходимо создать экземлпяр калькулятора и вывести в консоль
          * все операции согласно readme.md в папке calculator
          * */
 
