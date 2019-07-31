de337f0893d0f283099adfdcc80e5611bfd8cd7e
rose0317 <semenchuktltsu@gmail.com>
2019-07-31 15:37:49 +0400

    Some reformat code

--- src/objects/dns/employees/Employee.java
+++ src/objects/dns/employees/Employee.java
@@ -2,7 +2,6 @@ package objects.dns.employees;
 
 public class Employee extends Human {
     private String seniority;
-    private boolean banhammer=false;
 
     public Employee(String firstName, String lastName, int age, String seniority) {
         super(firstName, lastName, age);
@@ -12,7 +11,7 @@ public class Employee extends Human {
     @Override
     void doWork() {
         this.introduceYourself();
-        if(!seniority.equals("Кладовщик")) {
+        if (!seniority.equals("Кладовщик")) {
             System.out.println("Занимается продажей, выкладыванием товара и выполняет работу за Бориса. Универсальный продавец!");
         } else {
             System.out.println("Сортирует товар на складе, жестко и принципиально не прикрывает Бориса перед региональным менеджером.");
