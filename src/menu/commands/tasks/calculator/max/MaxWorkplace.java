dc3dd385ddecfbea58d5eeb379426fa3a88388cc
chris_barnes@mail.ru <ravenous888>
2019-08-07 10:26:01 +0400

    Калькулятор: исправлены замечания

--- src/menu/commands/tasks/calculator/max/MaxWorkplace.java
+++ src/menu/commands/tasks/calculator/max/MaxWorkplace.java
@@ -3,12 +3,11 @@ package menu.commands.tasks.calculator.max;
 import menu.commands.tasks.calculator.Workplace;
 
 public class MaxWorkplace implements Workplace {
-
     @Override
     public void start() {
 
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(40, 10));
+        System.out.println(calculator.sum(22, 22));
         System.out.println(calculator.dif(40, 25));
         System.out.println(calculator.div(50, 23));
         System.out.println(calculator.multi(32, 120));
