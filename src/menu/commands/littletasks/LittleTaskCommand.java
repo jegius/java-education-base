69b3481197f276d2b216591db3d8a9810fd17442
alexbossss <alexperminov94@gmail.com>
2019-09-17 22:10:10 +0400

    удалил readme, добавил команду

--- src/menu/commands/littletasks/LittleTaskCommand.java
+++ src/menu/commands/littletasks/LittleTaskCommand.java
@@ -2,6 +2,7 @@ package menu.commands.littletasks;
 
 import menu.Command;
 import menu.MainMenuCommand;
+import menu.commands.littletasks.alexninetyninebottles.AlexNinetyNineBottles;
 import menu.commands.littletasks.ninetyninebottles.NinetyNineBottles;
 import menu.commands.littletasks.nurseryforcats.NurseryForCats;
 import menu.commands.littletasks.simpleenum.Executor;
@@ -14,7 +15,8 @@ public class LittleTaskCommand implements Command {
 
     private static LittleTaskCommand instance;
 
-    private LittleTaskCommand() {}
+    private LittleTaskCommand() {
+    }
 
     public static synchronized LittleTaskCommand getInstance() {
         if (instance == null) {
@@ -28,6 +30,7 @@ public class LittleTaskCommand implements Command {
         MenuUtils.printSeparator();
         MenuUtils.printOption("1", "Run ninety nine bottles.");
         MenuUtils.printOption("2", "Run cat service.");
+        MenuUtils.printOption("3", "Run Alex bottles");
         MenuUtils.printOption("9", "Run Ivan simple enum.");
         MenuUtils.printOption("0", "Back");
         MenuUtils.printSeparator();
@@ -44,6 +47,9 @@ public class LittleTaskCommand implements Command {
                 return this;
             case 2:
                 NurseryForCats.run();
+                return this;
+            case 3:
+                AlexNinetyNineBottles.run();
                 return this;
             case 9:
                 TaskStore
