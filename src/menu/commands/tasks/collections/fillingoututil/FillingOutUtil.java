7992273ae5c597122b8cce4481d576e50d7106a3
Alekseev <alekseev@haulmont.com>
2019-10-22 01:44:42 +0400

    Поправлен FillingOutUtil

--- src/menu/commands/tasks/collections/fillingoututil/FillingOutUtil.java
+++ src/menu/commands/tasks/collections/fillingoututil/FillingOutUtil.java
@@ -1,14 +1,13 @@
 package menu.commands.tasks.collections.fillingoututil;
 
 import menu.commands.tasks.collections.List;
-import menu.commands.tasks.collections.StubList;
 
 public class FillingOutUtil {
 
     /**
      * Этап 3
      * */
-    public StubList fillList(List providedList, int length) throws Exception {
+    public List fillList(List providedList, int length) throws Exception {
         throw new Exception("No realization for this method!");
     }
 }
