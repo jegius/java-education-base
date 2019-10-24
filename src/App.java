2778ed422e42dd96e431ae45dff0b2aeb23a098a
alexbossss <alexperminov94@gmail.com>
2019-10-24 17:48:00 +0400

    19 finished second stage alex

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
 import menu.ConsoleMenu;
 import menu.MainMenuCommand;
+import menu.commands.tasks.collections.arraylist.AlexArrayList;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        AlexArrayList ales = new AlexArrayList();
+        ales.add(1222);
+        ales.add(1234);
+        ales.add(153);
+        ales.quickSort();
+        for (int i = 0; i < 3; i++) {
+            System.out.println(ales.get(i));
+        }
         ConsoleMenu menu = new ConsoleMenu(MainMenuCommand.getInstance());
         menu.run();
     }
