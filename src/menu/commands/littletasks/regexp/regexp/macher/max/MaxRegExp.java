22f7a31645068515e589b7f5205d2548faada68d
chris_barnes@mail.ru <ravenous888>
2019-12-03 19:28:15 +0400

    36 - regExp

--- src/menu/commands/littletasks/regexp/regexp/macher/max/MaxRegExp.java
+++ src/menu/commands/littletasks/regexp/regexp/macher/max/MaxRegExp.java
@@ -2,7 +2,8 @@ package menu.commands.littletasks.regexp.regexp.macher.max;
 
 public enum MaxRegExp {
     NUMBERS("(\\b4[5-9][6-9]\\b|\\b4[6-9][0-9]\\b|\\b[5-9][0-9][0-9]\\b|\\b1[0-1][0-9][0-9]\\b|1200\\b)"),
-    WORDS(""),
+    WORDS("\\b[lL]\\w+[mM]\\b"),
+    SENTENCE("");
 
     private String regExp;
 
