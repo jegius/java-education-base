35ab15e4c9c2440b65737a07b65b989d0d256569
chris_barnes@mail.ru <ravenous888>
2019-12-08 14:10:42 +0400

    36 - regExp

--- src/menu/commands/littletasks/regexp/regexp/validator/max/MaxValidatorType.java
+++ src/menu/commands/littletasks/regexp/regexp/validator/max/MaxValidatorType.java
@@ -1,9 +1,9 @@
 package menu.commands.littletasks.regexp.regexp.validator.max;
 
 public enum MaxValidatorType {
-    EMAIL("[]"),
-    PHONE(""),
-    CAR_NUMBER(""),
+    EMAIL("^(.)+@(.)+\\..+$"),
+    PHONE("^((8|\\+7|\\+44)[\\-]?)?(\\(?\\d{3}\\)?[\\-]?)?[\\d\\-]{7,10}$"),
+    CAR_NUMBER("^[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}|\\d{2,3}"),
     IP_ADDRESS("");
 
     private String regExp;
