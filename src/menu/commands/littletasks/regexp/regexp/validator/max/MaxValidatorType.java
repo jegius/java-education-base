03d80528d1fe265c618f4fffb95ea797b4e3c2f0
chris_barnes@mail.ru <ravenous888>
2020-01-02 16:44:45 +0400

    36 - RegExp. Исправил замечания

--- src/menu/commands/littletasks/regexp/regexp/validator/max/MaxValidatorType.java
+++ src/menu/commands/littletasks/regexp/regexp/validator/max/MaxValidatorType.java
@@ -2,7 +2,7 @@ package menu.commands.littletasks.regexp.regexp.validator.max;
 
 public enum MaxValidatorType {
     EMAIL("^(.)+@(.)+\\..+$"),
-    PHONE("^((8|\\+7|\\+44)[\\-]?)?(\\(?\\d{3}\\)?[\\-]?)?[\\d\\-]{7,10}$"),
+    PHONE("^(?:\\+\\d{2}|8)[\\-]?(\\(?\\d{3}\\)?[\\-]?)?[\\d\\-]{7,10}$"),
     CAR_NUMBER("^[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}|\\d{2,3}"),
     IP_ADDRESS("([0-9]{1,3}[\\.]){3}[0-9]{1,3}");
 
