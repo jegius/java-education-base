d77678ea1d4a0f1ff01e217537c71ec69396b911
Andrew <kozyrin.ai@gmail.com>
2021-04-30 10:38:59 +0400

    Andrew create generic task.

---
+++ src/menu/commands/tasks/generics/GenericTaskCommandAndrew.java
@@ -0,0 +1,53 @@
+package menu.commands.tasks.generics;
+
+import menu.Command;
+import menu.commands.tasks.TaskMenuCommand;
+import menu.commands.tasks.generics.andrew.Battlefield;
+import menu.commands.tasks.generics.andrew.Castle;
+import menu.commands.tasks.generics.andrew.Horde;
+import menu.utils.MenuUtils;
+
+import java.util.Scanner;
+
+public class GenericTaskCommandAndrew implements Command {
+    private static GenericTaskCommandAndrew instance;
+
+    public static synchronized GenericTaskCommandAndrew getInstance() {
+        if (instance == null) {
+            instance = new GenericTaskCommandAndrew();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public Command execute() {
+        MenuUtils.printSeparator();
+        MenuUtils.printOption("1", "Generate Castle troopers");
+        MenuUtils.printOption("2", "Generate Horde grunts");
+        MenuUtils.printOption("3", "Show Battlefield");
+        MenuUtils.printOption("0", "Back");
+        MenuUtils.printSeparator();
+        MenuUtils.printPrompt();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Castle.getInstance().showCastle();
+                return this;
+            case 2:
+                Horde.getInstance().showHorde();
+                return this;
+            case 3:
+                Battlefield.getInstance().showBattlefield();
+                return this;
+            case 0:
+                return TaskMenuCommand.getInstance();
+            default:
+                System.out.println("Unexpected command!");
+                return this;
+        }
+    }
+}
---
+++ src/menu/commands/tasks/generics/andrew/ArrayListGeneric.java
@@ -0,0 +1,50 @@
+package menu.commands.tasks.generics.andrew;
+
+public class ArrayListGeneric<T> {
+    private Object[] values = new Object[ArrayListGenericEnum.INIT_SIZE.getValue()];
+    private int size = 0;
+
+    public void add(T item) {
+        if (size == values.length - 1)
+            resize(values.length * 2);
+        values[size++] = item;
+    }
+
+    public T get(int index) {
+        return (T) values[index];
+    }
+
+    public Object set(Object o, int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Object oldObject = values[index];
+        values[index] = o;
+        return oldObject;
+    }
+
+    public void remove(int index) {
+        for (int i = index; i < size; i++)
+            values[i] = values[i + 1];
+        values[size] = null;
+        size--;
+        if (values.length > ArrayListGenericEnum.INIT_SIZE.getValue() && size < values.length / ArrayListGenericEnum.CUT_RATE.getValue())
+            resize(values.length / 2);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void resize(int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(values, 0, newArray, 0, size);
+        values = newArray;
+    }
+
+    public void printElements() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(get(i));
+        }
+    }
+}
