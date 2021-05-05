d482cfbcf444c861a300947d163c05abd9422f8b
Andrew <kozyrin.ai@gmail.com>
2021-05-05 08:19:54 +0400

    Переделал switch на if, создал новый enum для string значений, переименовал MinionEnum в MinionIntEnum, удалил string из MinionIntEnum. Замениел string на enum в Castle и Horde.

---
+++ src/menu/commands/tasks/generics/andrew/generator/MinionIntEnum.java
@@ -0,0 +1,18 @@
+package menu.commands.tasks.generics.andrew.generator;
+
+public enum MinionIntEnum {
+    MIN_AGE(18),
+    MAX_AGE(50),
+    MIN_POWER(15),
+    MAX_POWER(150),
+    MINION_HP(100);
+    private final int value;
+
+    MinionIntEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
