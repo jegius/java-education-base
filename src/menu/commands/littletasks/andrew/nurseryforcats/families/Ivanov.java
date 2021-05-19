d9e78b91dbc83c50b1c06d79364b305a0ceb1814
Andrew <kozyrin.ai@gmail.com>
2021-05-19 11:35:31 +0400

    Сделал наследование фамилий от абстрактного класса Family NurseryForCats

--- src/menu/commands/littletasks/andrew/nurseryforcats/families/Ivanov.java
+++ src/menu/commands/littletasks/andrew/nurseryforcats/families/Ivanov.java
@@ -1,21 +1,8 @@
 package menu.commands.littletasks.andrew.nurseryforcats.families;
 
-import menu.commands.littletasks.andrew.nurseryforcats.Cat;
-import menu.commands.littletasks.andrew.nurseryforcats.CatShelter;
-import menu.commands.littletasks.andrew.nurseryforcats.GenerateCats;
-
-import java.util.List;
-
-public class Ivanov implements FamilyCat{
-
-    private final List<Cat> cats;
+public class Ivanov extends Family {
 
     public Ivanov() {
-        cats = GenerateCats.generate();
-    }
-
-    @Override
-    public void sendToTheShelter(Class<?> cClass) {
-        CatShelter.printCats(cats, cClass);
+        this.cClass = this.getClass();
     }
 }
--- src/menu/commands/littletasks/andrew/nurseryforcats/families/Petrov.java
+++ src/menu/commands/littletasks/andrew/nurseryforcats/families/Petrov.java
@@ -1,21 +1,8 @@
 package menu.commands.littletasks.andrew.nurseryforcats.families;
 
-import menu.commands.littletasks.andrew.nurseryforcats.Cat;
-import menu.commands.littletasks.andrew.nurseryforcats.CatShelter;
-import menu.commands.littletasks.andrew.nurseryforcats.GenerateCats;
-
-import java.util.List;
-
-public class Petrov implements FamilyCat {
-
-    private final List<Cat> cats;
+public class Petrov extends Family {
 
     public Petrov() {
-        cats = GenerateCats.generate();
-    }
-
-    @Override
-    public void sendToTheShelter(Class<?> cClass) {
-        CatShelter.printCats(cats, cClass);
+        this.cClass = this.getClass();
     }
 }
