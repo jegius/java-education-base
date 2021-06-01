558f8774e8e01637a09f4c1c512642a09c49ba9f
Andrew <kozyrin.ai@gmail.com>
2021-06-01 07:18:56 +0400

    Убрал ненужные геттеры сделал value final.

--- src/menu/commands/tasks/generics/andrew/ArrayListGenericEnum.java
+++ src/menu/commands/tasks/generics/andrew/ArrayListGenericEnum.java
@@ -5,22 +5,13 @@ public enum ArrayListGenericEnum {
     INIT_SIZE(16),
     CUT_RATE(4);
 
-    private int value;
-    private String field;
+    private final int value;
 
     ArrayListGenericEnum(int value) {
         this.value = value;
     }
 
-    ArrayListGenericEnum(String field) {
-        this.field = field;
-    }
-
     public int getValue() {
         return value;
-    }
-
-    public String getField() {
-        return field;
     }
 }
