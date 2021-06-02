83602b81a5d2009a1c0149654c758bb2418ea810
Andrew <kozyrin.ai@gmail.com>
2021-06-02 15:27:40 +0400

    SimpleEnum добавил рандомный выбор часов для билдера

--- src/menu/commands/littletasks/simpleenum/andrew/AndrewTask.java
+++ src/menu/commands/littletasks/simpleenum/andrew/AndrewTask.java
@@ -1,15 +1,22 @@
 package menu.commands.littletasks.simpleenum.andrew;
 
 import menu.commands.littletasks.simpleenum.Task;
-import menu.commands.littletasks.simpleenum.andrew.WatchFactory;
-import menu.commands.littletasks.simpleenum.andrew.WatchType;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class AndrewTask implements Task {
 
     @Override
     public void runTask() {
-        Objects.requireNonNull(WatchFactory.buildWatchByType(WatchType.DIGITAL_WATCH)).showTime();
+        WatchType getWatchType = randomWatchType();
+
+        Objects.requireNonNull(WatchFactory.buildWatchByType(getWatchType)).showTime();
+    }
+
+    public WatchType randomWatchType(){
+        Random random = new Random();
+        int typeId = random.nextInt(WatchType.values().length);
+        return WatchType.values()[typeId];
     }
 }
